c3417876f6735bff24cd838b609f6dd4655494a0
umnovjp <umnovjp@yahoo.com>
2025-06-30 18:01:32 -0500

    developing fiveOnFive staff

--- assets/backup.js
+++ assets/backup.js
@@ -61,76 +61,7 @@ function getInputValue() {
            var shiftsURL = 'https://cors-anywhere.herokuapp.com/https://api.nhle.com/stats/rest/en/shiftcharts?cayenneExp=gameId=' + gameId;
             fetch(shiftsURL, { "method": "GET", "headers": {} })
               .then(function (response) { return response.json() })
-              .then(function (data_shifts) { console.log('I am in second shift then', data_shifts.data, shiftsArray[0]);
-                for (i=0;i<data_shifts.data.length;i++) {if (data_shifts.data[i].typeCode===517) {
-                  for (j=0;j<shiftsArray.length;j++) { for (k=0;k<3;k++) {
-                    if ((data_shifts.data[i].playerId===shiftsArray[j].playerId)&&(data_shifts.data[i].period===k+1)) {shiftsArray[j].shiftsObject.startTime[k] = shiftsArray[j].shiftsObject.startTime[k] + ', ' + data_shifts.data[i].startTime, shiftsArray[j].shiftsObject.endTime[k] = shiftsArray[j].shiftsObject.endTime[k] + ', ' + data_shifts.data[i].endTime}}
-                      }
-                }}
-                console.log(shiftsArray, shiftsArray[2].shiftsObject);
-                // ok I have analog of dArray and fArray. Then I created dArrayTemp and fArayTemp in lines 100, 101. Just combined all d shifts and all f shifts. 
-                // then I created an array of 3 x 1200 0s. that was dArrayTemp2 and fArrayTemp2 in line 105. 
-                // then I add 1 for each second of the shift. So in the end I know when it was 2D + 3F
-                // then I ran cycle to eliminate end shift start shift pairs ???
-                  tempArrayDH=[[],[],[]]; tempArrayFH=[[],[],[]];tempArrayGH=[[],[],[]];tempArrayDA=[[],[],[]];tempArrayFA=[[],[],[]];tempArrayGA=[[],[],[]];tempArrayGH1=[[],[],[]];tempArrayDH1=[[],[],[]];tempArrayFH1=[[],[],[]];tempArrayDA1=[[],[],[]];tempArrayDA2=[[],[],[]];tempArrayFA1=[[],[],[]];tempArrayGA1=[[],[],[]];
-                  tempArrayDA3=[[],[],[]]; tempArrayFA2=[[],[],[]]; 
-                  for (i=0;i<1200;i++) { for (j=0;j<3;j++) {
-                    tempArrayFH[j].push(0); tempArrayDH[j].push(0); tempArrayGH[j].push(1); tempArrayDA[j].push(0); tempArrayFA[j].push(0); tempArrayGA[j].push(1)
-                    tempArrayDA3[j].push(0); tempArrayFA2[j].push(0)
-                  }}
-                  // start DH, FH, GA, GH, DA, FA
-                  for (i=0;i<shiftsArray.length;i++) { for (h=0;h<3;h++) { // i is players but h is 3 periods                    
-                    {if (shiftsArray[i].shiftsObject.startTime[h].length>0) {
-                      startTimeArray=shiftsArray[i].shiftsObject.startTime[h].split(',');
-                    endTimeArray=shiftsArray[i].shiftsObject.endTime[h].split(',');
-                    for (j=1;j<startTimeArray.length;j++) {startSeconds=Number(startTimeArray[j].split(':')[0])*60+Number(startTimeArray[j].split(':')[1]);
-                    endSeconds=Number(endTimeArray[j].split(':')[0])*60+Number(endTimeArray[j].split(':')[1]);
-                    for (k=startSeconds;k<endSeconds;k++) {
-                      if ((shiftsArray[i].position==='D')&&(shiftsArray[i].team==='H')) {tempArrayDH[h][k]=tempArrayDH[h][k]+1}
-                      else if ((shiftsArray[i].position==='F')&&(shiftsArray[i].team==='H')) {tempArrayFH[h][k]=tempArrayFH[h][k]+1}
-                      else if ((shiftsArray[i].position==='G')&&(shiftsArray[i].team==='H')) {tempArrayGH[h][k]=tempArrayGH[h][k]+1}
-                      else if ((shiftsArray[i].position==='D')&&(shiftsArray[i].team==='A')) {tempArrayDA[h][k]=tempArrayDA[h][k]+1; tempArrayDA3[h][k]=tempArrayDA2[h][k]+1}
-                      else if ((shiftsArray[i].position==='F')&&(shiftsArray[i].team==='A')) {tempArrayFA[h][k]=tempArrayFA[h][k]+1; tempArrayFA2[h][k]=tempArrayFA2[h][k]+1}
-                      else if ((shiftsArray[i].position==='G')&&(shiftsArray[i].team==='A')) {tempArrayGA[h][k]=tempArrayGA[h][k]+1}
-                    }}}}
-                }} // end i,h cycle
-                
-                  for (i=0;i<1200;i++) { for (j=0;j<3;j++) {if (tempArrayDH[j][i]===tempArrayDH[j][i+1]) {delete(tempArrayDH[j][i])}
-                  if (tempArrayFH[j][i]===tempArrayFH[j][i+1]) {delete(tempArrayFH[j][i])}
-                  if (tempArrayGH[j][i]===tempArrayGH[j][i+1]) {delete(tempArrayGH[j][i])}
-                  if (tempArrayDA[j][i]===tempArrayDA[j][i+1]) {delete(tempArrayDA[j][i])}
-                  if (tempArrayFA[j][i]===tempArrayFA[j][i+1]) {delete(tempArrayFA[j][i])}
-                  if (tempArrayGA[j][i]===tempArrayGA[j][i+1]) {delete(tempArrayGA[j][i])}
-                }}
-                // console.log(tempArrayDA, tempArrayFA)
-                  for (i=0;i<1200;i++) { for (j=0;j<3;j++) {if (!tempArrayDH[j][i]) {} else {tempArrayDH1[j].push(tempArrayDH[j][i],i)}
-                  if (!tempArrayFH[j][i]) {} else {tempArrayFH1[j].push(tempArrayFH[j][i],i)}
-                  if (!tempArrayGH[j][i]) {} else {tempArrayGH1[j].push(tempArrayGH[j][i],i)}
-                  if (!tempArrayDA[j][i]) {} else {tempArrayDA1[j].push(tempArrayDA[j][i],i); tempArrayDA2[j].push(tempArrayDA[j][i],i)}
-                  if (!tempArrayFA[j][i]) {} else {tempArrayFA1[j].push(tempArrayFA[j][i],i)}
-                  if (!tempArrayGA[j][i]) {} else {tempArrayGA1[j].push(tempArrayGA[j][i],i)}
-                }}
-                console.log(tempArrayDH1, tempArrayFH1, tempArrayGH1, tempArrayDA2, tempArrayFA1, tempArrayGA1);
-                fiveOnFive=[[],[],[]]; fiveOnFive3=tempArrayDA1; fiveOnFive4=tempArrayFA1; fiveOnFive5=tempArrayGA1;
-                // fiveOnFive3=fiveOnFive2;
-                for (i=0;i<3;i++) {for (j=tempArrayDA1[i].length/2-1; j>0; j--) {if (fiveOnFive3[i][2*j+1]-fiveOnFive3[i][2*j-1]<4) {
-                  tempArray1=fiveOnFive3[i].slice(0,2*j-2); tempArray2=fiveOnFive3[i].slice(2*j+2);
-                  if (i===0) {console.log(tempArray1, tempArray2)}
-                     fiveOnFive3[i]=tempArray1.concat(tempArray2)
-                }}
-                for (j=tempArrayFA1[i].length/2-1; j>0; j--) {if (fiveOnFive4[i][2*j+1]-fiveOnFive4[i][2*j-1]<4) {
-                  tempArray1=fiveOnFive4[i].slice(0,2*j-2); tempArray2=fiveOnFive4[i].slice(2*j+2);
-                  if (i===0) {console.log(tempArray1, tempArray2)}
-                     fiveOnFive4[i]=tempArray1.concat(tempArray2)
-                }}
-                for (j=tempArrayGA1[i].length/2-1; j>0; j--) {if (fiveOnFive5[i][2*j+1]-fiveOnFive5[i][2*j-1]<4) {
-                  tempArray1=fiveOnFive5[i].slice(0,2*j-2); tempArray2=fiveOnFive5[i].slice(2*j+2);
-                  if (i===0) {console.log(tempArray1, tempArray2)}
-                     fiveOnFive5[i]=tempArray1.concat(tempArray2)
-                }}
-              }
-          
-                console.log(fiveOnFive3, fiveOnFive4, fiveOnFive5)
+              .then(function (data_shifts) { 
 
               }); // end second .then shifts
           }); // end second .then gamecenter;
@@ -138,3 +69,17 @@ function getInputValue() {
     } // end second .then from getinputvalue
     );
 } // end getInput Value function 
+
+for (j = 0; j < 2; j++) {for (k = 0; k < fiveOnFive[i][j].length/2; k++)  {for (l = 0; l < fiveOnFive3[i][j].length/2; l++) {
+  if ((fiveOnFive[i][j][2*k] >= fiveOnFive3[i][j][2*l])&&(fiveOnFive[i][j][2*k+1] <= fiveOnFive3[i][j][2*l+1])) {fiveOnFive5[i][j].push(fiveOnFive[i][j][2*k], fiveOnFive[i][j][2*k+1])}
+  else if ((fiveOnFive[i][j][2*k] <= fiveOnFive3[i][j][2*l])&&(fiveOnFive[i][j][2*k+1] >= fiveOnFive3[i][j][2*l+1])) {fiveOnFive5[i][j].push(fiveOnFive3[i][j][2*l], fiveOnFive3[i][j][2*l+1])}
+  else if ((fiveOnFive[i][j][2*k]>=fiveOnFive3[i][j][2*l])&&(fiveOnFive[i][j][2*k+1]>=fiveOnFive3[i][j][2*l+1])&&(fiveOnFive[i][j][2*k]<fiveOnFive3[i][j][2*l+1])) {fiveOnFive5[i][j].push(fiveOnFive[i][j][2*k], fiveOnFive3[i][j][2*l+1])}
+  else if ((fiveOnFive[i][j][2*k]<=fiveOnFive3[i][j][2*l])&&(fiveOnFive[i][j][2*k+1]<=fiveOnFive3[i][j][2*l+1])&&(fiveOnFive[i][j][2*k+1]>fiveOnFive3[i][j][2*l])) {fiveOnFive5[i][j].push(fiveOnFive3[i][j][2*l], fiveOnFive[i][j][2*k+1])}
+}}} 
+
+for (i = 0; i < 2; i++) {for (j = 0; j < 3; j++) {for (k = 0; k < fiveOnFive[i][j].length/2; k++)  {for (l = 0; l < fiveOnFive3[i][j].length/2; l++) {
+  if ((fiveOnFive[i][j][2*k] >= fiveOnFive3[i][j][2*l])&&(fiveOnFive[i][j][2*k+1] <= fiveOnFive3[i][j][2*l+1])) {fiveOnFive5[i][j].push(fiveOnFive[i][j][2*k], fiveOnFive[i][j][2*k+1])}
+  else if ((fiveOnFive[i][j][2*k] <= fiveOnFive3[i][j][2*l])&&(fiveOnFive[i][j][2*k+1] >= fiveOnFive3[i][j][2*l+1])) {fiveOnFive5[i][j].push(fiveOnFive3[i][j][2*l], fiveOnFive3[i][j][2*l+1])}
+  else if ((fiveOnFive[i][j][2*k]>=fiveOnFive3[i][j][2*l])&&(fiveOnFive[i][j][2*k+1]>=fiveOnFive3[i][j][2*l+1])&&(fiveOnFive[i][j][2*k]<fiveOnFive3[i][j][2*l+1])) {fiveOnFive5[i][j].push(fiveOnFive[i][j][2*k], fiveOnFive3[i][j][2*l+1])}
+  else if ((fiveOnFive[i][j][2*k]<=fiveOnFive3[i][j][2*l])&&(fiveOnFive[i][j][2*k+1]<=fiveOnFive3[i][j][2*l+1])&&(fiveOnFive[i][j][2*k+1]>fiveOnFive3[i][j][2*l])) {fiveOnFive5[i][j].push(fiveOnFive3[i][j][2*l], fiveOnFive[i][j][2*k+1])}
+}}}} 
