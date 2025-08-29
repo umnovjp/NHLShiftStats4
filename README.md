e3ee368fe451eb333b402c54dcb7cd07664fb70e
umnovjp <umnovjp@yahoo.com>
2025-08-28 23:35:53 -0500

    again did not have time to write a new code because of home repair just deleted some staff

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # NHLShiftStats4
 This repo is update on my previous repo located here: https://github.com/umnovjp/NHLShiftsStats3. I just started doing staff in a different way. Using objects and not arrays. Does not improve too much in fact using object takes more lines than arrays. Copied old script
 ## Future Development
-That's developing pretty well. I am progressing. Next step is to find new lines array. Just cannot create this script. Expected to be easy. Today attempting to do update from different laptop. 
+That's developing pretty well. I am progressing. Next step is to find new lines array. Just cannot create this script. Expected to be easy.
--- assets/script.js
+++ assets/script.js
@@ -41,29 +41,17 @@ function getInputValue() { var inputVal = document.getElementById('datepicker').
                 const homeF = []; const awayF = []; const homeD = []; const awayD = []; const homeG = []; const awayG = []; const playerIdArray = []; // let playerIdeObject = {a: 1}; const hasKeyId = true;
                 // to delete playerIdeObject
                 var obj = data.playerByGameStats.homeTeam.forwards; var keys = Object.keys(obj); playerIdeObject = {};
-                for (i = 0; i < keys.length; i++) { var val = obj[keys[i]]; homeF.push(val.playerId, val.sweaterNumber, val.name.default); playerIdArray.push(val.playerId, [[], [], []]);
-                 // keyId = val.playerId; playerIdeObject[keyId] = []
-                }
+                for (i = 0; i < keys.length; i++) { var val = obj[keys[i]]; homeF.push(val.playerId, val.sweaterNumber, val.name.default); playerIdArray.push(val.playerId, [[], [], []])}
                 var obj = data.playerByGameStats.homeTeam.defense; var keys = Object.keys(obj);
-                for (i = 0; i < keys.length; i++) { var val = obj[keys[i]]; homeD.push(val.playerId, val.sweaterNumber, val.name.default); playerIdArray.push(val.playerId, [[], [], []])
-                 // keyId = val.playerId; playerIdeObject[keyId] = []
-                }
+                for (i = 0; i < keys.length; i++) { var val = obj[keys[i]]; homeD.push(val.playerId, val.sweaterNumber, val.name.default); playerIdArray.push(val.playerId, [[], [], []])}
                 var obj = data.playerByGameStats.homeTeam.goalies; var keys = Object.keys(obj);
-                for (i = 0; i < keys.length; i++) { var val = obj[keys[i]]; homeG.push(val.playerId, val.sweaterNumber, val.name.default); playerIdArray.push(val.playerId, [[], [], []])
-                 // keyId = val.playerId; playerIdeObject[keyId] = []
-                }
+                for (i = 0; i < keys.length; i++) { var val = obj[keys[i]]; homeG.push(val.playerId, val.sweaterNumber, val.name.default); playerIdArray.push(val.playerId, [[], [], []])}
                 var obj = data.playerByGameStats.awayTeam.forwards; var keys = Object.keys(obj);
-                for (i = 0; i < keys.length; i++) { var val = obj[keys[i]]; awayF.push(val.playerId, val.sweaterNumber, val.name.default); playerIdArray.push(val.playerId, [[], [], []])
-                 // keyId = val.playerId; playerIdeObject[keyId] = []
-                }
+                for (i = 0; i < keys.length; i++) { var val = obj[keys[i]]; awayF.push(val.playerId, val.sweaterNumber, val.name.default); playerIdArray.push(val.playerId, [[], [], []])}
                 var obj = data.playerByGameStats.awayTeam.defense; var keys = Object.keys(obj);
-                for (i = 0; i < keys.length; i++) {var val = obj[keys[i]]; awayD.push(val.playerId, val.sweaterNumber, val.name.default); playerIdArray.push(val.playerId, [[], [], []]);
-                 // keyId = val.playerId; // playerIdeObject[keyId] = []
-                }
+                for (i = 0; i < keys.length; i++) {var val = obj[keys[i]]; awayD.push(val.playerId, val.sweaterNumber, val.name.default); playerIdArray.push(val.playerId, [[], [], []])}
                 var obj = data.playerByGameStats.awayTeam.goalies; var keys = Object.keys(obj);
-                for (i = 0; i < keys.length; i++) {var val = obj[keys[i]]; awayG.push(val.playerId, val.sweaterNumber, val.name.default); playerIdArray.push(val.playerId, [[], [], []]);
-                 //  keyId = val.playerId; // playerIdeObject[keyId] = []
-                }
+                for (i = 0; i < keys.length; i++) {var val = obj[keys[i]]; awayG.push(val.playerId, val.sweaterNumber, val.name.default); playerIdArray.push(val.playerId, [[], [], []])}
 
                 var shiftsURL = 'https://cors-anywhere.herokuapp.com/https://api.nhle.com/stats/rest/en/shiftcharts?cayenneExp=gameId=' + gameId;
                 fetch(shiftsURL, { "method": "GET", "headers": {} })
