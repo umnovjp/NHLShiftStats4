38001ad081fa8992cce2f2c662ffc58766ff8537
umnovjp <umnovjp@yahoo.com>
2025-10-08 23:36:40 -0500

    very minor changes tonight lack of time

--- assets/script.js
+++ assets/script.js
@@ -7,15 +7,12 @@ function getInputValue() { var inputVal = document.getElementById('datepicker').
     .then(function (response) {return response.json()})
     .then(function (data) {console.log('I am in schedule then');
       var numberOfGames = data.gameWeek[0].games.length; scheduleContent.textContent = '';
-      for (var i = 0; i < numberOfGames; i++) { var gameName = document.createElement('button');
-        gameName.setAttribute('id', 'game' + i); var idx = gameName.getAttribute('id');
+      for (var i = 0; i < numberOfGames; i++) { var gameName = document.createElement('button'); gameName.setAttribute('id', 'game' + i); var idx = gameName.getAttribute('id');
         gameName.innerHTML = 'Game ' + i + ': ' + data.gameWeek[0].games[i].awayTeam.abbrev + ' vs ' + data.gameWeek[0].games[i].homeTeam.abbrev;
-        document.getElementById('schedule').appendChild(gameName); gameName.addEventListener('click', displayGameData);
-      }
+        document.getElementById('schedule').appendChild(gameName); gameName.addEventListener('click', displayGameData)}
 
       function displayGameData(event) {idx = event.currentTarget; idxString = event.currentTarget.textContent;
-        idxArray = idxString.split(':'); idxNumber = idxArray[0].split(' '); gameNumber = idxNumber[1];
-        const gameId = data.gameWeek[0].games[gameNumber].id; console.log(gameId);
+        idxArray = idxString.split(':'); idxNumber = idxArray[0].split(' '); gameNumber = idxNumber[1]; const gameId = data.gameWeek[0].games[gameNumber].id; console.log(gameId);
         var requestURL = 'https://cors-anywhere.herokuapp.com/https://api-web.nhle.com/v1/gamecenter/' + gameId + '/boxscore';
         fetch(requestURL, { "method": "GET", "headers": {}}
           )
@@ -32,8 +29,7 @@ function getInputValue() { var inputVal = document.getElementById('datepicker').
                     console.log(i, data.awayTeam.abbrev, data_standings.standings[i].wins, data_standings.standings[i].losses, data_standings.standings[i].otLosses)}
                   else if (data_standings.standings[i].teamAbbrev.default === data.homeTeam.abbrev) {
                     standingsArray.push(data.homeTeam.abbrev, data_standings.standings[i].wins, data_standings.standings[i].losses, data_standings.standings[i].otLosses)
-                    console.log(i, data.homeTeam.abbrev, data_standings.standings[i].wins, data_standings.standings[i].losses, data_standings.standings[i].otLosses)
-                  }
+                    console.log(i, data.homeTeam.abbrev, data_standings.standings[i].wins, data_standings.standings[i].losses, data_standings.standings[i].otLosses)}
                   else (console.log('No Such Abbrev'))}
                 var gameTitle = document.createElement('h2'); gameTitle.textContent = '';
                 gameTitle.innerHTML = 'You are watching stats for ' + data.awayTeam.abbrev + standingsArray[0] + ' W ' + standingsArray[1] + ' L ' + standingsArray[2] + ' O at ' + data.homeTeam.abbrev + standingsArray[3] + ' W ' + standingsArray[4] + ' L ' + standingsArray[5] + ' O game';
