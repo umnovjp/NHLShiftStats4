c231b4f76dd02d4fc5db9dbe6bc45e9207477ce1
umnovjp <umnovjp@yahoo.com>
2025-11-27 04:12:36 -0600

    added fiveOnFive6 to function lineByLine2 but thre is no difference for game 3 11/21 and stars 11/25 will look further

--- assets/script.js
+++ assets/script.js
@@ -319,7 +319,7 @@ function getInputValue() { var inputVal = document.getElementById('datepicker').
 
                     // q,r,s are forwards on home team, t,u,v are forwards on away team values are 0...11 if 12F or 0...10 if 11F
                     function lineByLine2(q,r,s,t,u,v) { // created an array with 15 elements need to add more
-                    shiftsLine2=[]; for (p=0;p<18;p++) {shiftsLine2.push([])} shiftsLine2[16]=[[],[],[]] // to add 3 more p in this line
+                    shiftsLine2=[]; for (p=0;p<18;p++) {shiftsLine2.push([])} shiftsLine2[16]=[[],[],[]] 
                     for (n=0;n<3;n++) { for (l=0;l<fArray[0][q][n].length/2;l++) { for (m=0;m<fArray[0][r][n].length/2;m++) {if ((fArray[0][r][n][2*m]>=fArray[0][q][n][2*l])&&(fArray[0][r][n][2*m]<=fArray[0][q][n][2*l+1]))
                     {if (fArray[0][r][n][2*m+1]>=fArray[0][q][n][2*l+1]) {shiftsLine2[n].push(fArray[0][r][n][2*m], fArray[0][q][n][2*l+1]) }
                     else { shiftsLine2[n].push(fArray[0][r][n][2*m], fArray[0][r][n][2*m+1]) }}
@@ -333,7 +333,7 @@ function getInputValue() { var inputVal = document.getElementById('datepicker').
                       else if ((fArray[0][s][n][2*m]<=shiftsLine2[n][2*l])&&(fArray[0][s][n][2*m+1]>=shiftsLine2[n][2*l])) {
                       if (fArray[0][s][n][2*m+1]>=shiftsLine2[n][2*l+1]) { shiftsLine2[n+3].push(shiftsLine2[n][2*l], shiftsLine2[n][2*l+1]) }
                       else {shiftsLine2[n+3].push(shiftsLine2[n][2*l], fArray[0][s][n][2*m+1])}}
-                    }} // end second m,l loop
+                    }} // end second m,l loop h team
                     for (l=0;l<fArray[1][t][n].length/2;l++) { // console.log('error0', fArray[1], 'n=', n, 'l=', l);
                       for (m=0;m<fArray[1][u][n].length/2;m++) {if ((fArray[1][u][n][2*m]>=fArray[1][t][n][2*l])&&(fArray[1][u][n][2*m]<=fArray[1][t][n][2*l+1]))
                     {if (fArray[1][u][n][2*m+1]>=fArray[1][t][n][2*l+1]) {shiftsLine2[6+n].push(fArray[1][u][n][2*m], fArray[1][t][n][2*l+1]) }
@@ -350,18 +350,35 @@ function getInputValue() { var inputVal = document.getElementById('datepicker').
                     else if ((fArray[1][v][n][2*m]<=shiftsLine2[6+n][2*l])&&(fArray[1][v][n][2*m+1]>=shiftsLine2[6+n][2*l])) {
                     if (fArray[1][v][n][2*m+1]>=shiftsLine2[6+n][2*l+1]) { shiftsLine2[9+n].push(shiftsLine2[6+n][2*l], shiftsLine2[6+n][2*l+1]) }
                     else {shiftsLine2[9+n].push(shiftsLine2[6+n][2*l], fArray[1][v][n][2*m+1])}}
-                    }} // end second away m,l loop
-
-                    shiftsLine2[12].push([]);
-                    for (l=0;l<shiftsLine2[3+n].length/2;l++) {for (m=0;m<shiftsLine2[9+n].length/2;m++) {if ((shiftsLine2[9+n][2*m]>=shiftsLine2[3+n][2*l])&&(shiftsLine2[9+n][2*m]<=shiftsLine2[3+n][2*l+1])){
-                    if (shiftsLine2[9+n][2*m+1]>=shiftsLine2[3+n][2*l+1]){shiftsLine2[12][n].push(shiftsLine2[9+n][2*m], shiftsLine2[3+n][2*l+1])}
-                    else { shiftsLine2[12][n].push(shiftsLine2[9+n][2*m], shiftsLine2[9+n][2*m+1]) }}
-                    else if ((shiftsLine2[9+n][2*m]<=shiftsLine2[3+n][2*l])&&(shiftsLine2[9+n][2*m+1]>=shiftsLine2[3+n][2*l])) {
-                    if (shiftsLine2[9+n][2*m+1]>=shiftsLine2[3+n][2*l+1]) { shiftsLine2[12][n].push(shiftsLine2[3+n][2*l], shiftsLine2[3+n][2*l+1]) }
-                      else {shiftsLine2[12][n].push(shiftsLine2[3+n][2*l], shiftsLine2[9+n][2*m+1])}}
-                    }} // end m,l loop line vs line
+                    }} // end second away team m,l loop
 
                     // to add loop here
+                    for (l=0;l<shiftsLine2[9+n].length/2;l++) {for (m=0;m<fiveOnFive6[n].length/2;m++) {if ((fiveOnFive6[n][2*m]>=shiftsLine2[9+n][2*l])&&(fiveOnFive6[n][2*m]<=shiftsLine2[9+n][2*l+1])){
+                    if (fiveOnFive6[n][2*m+1]>=shiftsLine2[9+n][2*l+1]){shiftsLine2[13+n].push(fiveOnFive6[n][2*m],shiftsLine2[9+n][2*l+1])}
+                    else { shiftsLine2[13+n].push(fiveOnFive6[n][2*m], fiveOnFive6[n][2*m+1]) }}
+                    else if ((fiveOnFive6[n][2*m]<=shiftsLine2[9+n][2*l])&&(fiveOnFive6[n][2*m+1]>=shiftsLine2[9+n][2*l])) {
+                    if (fiveOnFive6[n][2*m+1]>=shiftsLine2[9+n][2*l+1]) { shiftsLine2[13+n].push(shiftsLine2[9+n][2*l], shiftsLine2[9+n][2*l+1]) }
+                      else {shiftsLine2[13+n].push(shiftsLine2[9+n][2*l], fiveOnFive6[n][2*m+1])}}
+                    }} // end m,l loop fiveOnFive6
+
+                    shiftsLine2[12].push([]);
+                    for (l=0;l<shiftsLine2[3+n].length/2;l++) {for (m=0;m<shiftsLine2[13+n].length/2;m++) {if ((shiftsLine2[13+n][2*m]>=shiftsLine2[3+n][2*l])&&(shiftsLine2[13+n][2*m]<=shiftsLine2[3+n][2*l+1])){
+                    if (shiftsLine2[13+n][2*m+1]>=shiftsLine2[3+n][2*l+1]){shiftsLine2[12][n].push(shiftsLine2[13+n][2*m], shiftsLine2[3+n][2*l+1])}
+                    else { shiftsLine2[12][n].push(shiftsLine2[13+n][2*m], shiftsLine2[13+n][2*m+1]) }}
+                    else if ((shiftsLine2[13+n][2*m]<=shiftsLine2[3+n][2*l])&&(shiftsLine2[13+n][2*m+1]>=shiftsLine2[3+n][2*l])) {
+                    if (shiftsLine2[13+n][2*m+1]>=shiftsLine2[3+n][2*l+1]) { shiftsLine2[12][n].push(shiftsLine2[3+n][2*l], shiftsLine2[3+n][2*l+1]) }
+                      else {shiftsLine2[12][n].push(shiftsLine2[3+n][2*l], shiftsLine2[13+n][2*m+1])}}
+                    }} // end m,l loop line vs line
+
+                    // // to add loop here
+                    // for (l=0;l<shiftsLine2[12][n].length/2;l++) {for (m=0;m<fiveOnFive6[n].length/2;m++) {if ((fiveOnFive6[n][2*m]>=shiftsLine2[12][n][2*l])&&(fiveOnFive6[n][2*m]<=shiftsLine2[12][n][2*l+1])){
+                    // if (fiveOnFive6[n][2*m+1]>=shiftsLine2[12][n][2*l+1]){shiftsLine2[13+n].push(fiveOnFive6[n][2*m], shiftsLine2[12][n][2*l+1])}
+                    // else { shiftsLine2[13+n][n].push(fiveOnFive6[n][2*m], fiveOnFive6[n][2*m+1]) }}
+                    // else if ((fiveOnFive6[n][2*m]<=shiftsLine2[12][n][2*l])&&(fiveOnFive6[n][2*m+1]>=shiftsLine2[12][n][2*l])) {
+                    // if (fiveOnFive6[n][2*m+1]>=shiftsLine2[12][n][2*l+1]) { shiftsLine2[13+n].push(shiftsLine2[12][n][2*l], shiftsLine2[12][n][2*l+1]) }
+                    //   else {shiftsLine2[13+n].push(shiftsLine2[12][n][2*l], fiveOnFive6[n][2*m+1])}}
+                    // }} // end m,l loop fiveOnFive6
+
                     lineVsLineTime=0; lineVsLineShifts=0;
                     for (k=0;k<shiftsLine2[12][n].length/2;k++) { lineVsLineTime=lineVsLineTime+shiftsLine2[12][n][2*k+1]-shiftsLine2[12][n][2*k];
                     if (shiftsLine2[12][n][2*k+1]-shiftsLine2[12][n][2*k]>=10) {lineVsLineShifts=lineVsLineShifts+1}}
