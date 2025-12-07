acd71073ca8e4b524738b041ee2f437d3fb61d22
umnovjp <umnovjp@yahoo.com>
2025-12-06 23:45:53 -0600

    another minor update as I was dealing with different issues

--- assets/script.js
+++ assets/script.js
@@ -144,18 +144,18 @@ function getInputValue() { var inputVal = document.getElementById('datepicker').
                                   else { shiftsPair.push(fArray[h][k][i][2 * m], fArray[h][k][i][2 * m + 1]) }}
                                 else if ((fArray[h][k][i][2 * m] <= fArray[h][j][i][2 * l]) && (fArray[h][k][i][2 * m + 1] >= fArray[h][j][i][2 * l])) {
                                   if (fArray[h][k][i][2 * m + 1] >= fArray[h][j][i][2 * l + 1]) { shiftsPair.push(fArray[h][j][i][2 * l], fArray[h][j][i][2 * l + 1]) }
-                                  else {shiftsPair.push(fArray[h][j][i][2 * l], fArray[h][k][i][2 * m + 1])}
+                                  else {shiftsPair.push(fArray[h][j][i][2*l], fArray[h][k][i][2*m+1])}
                                 }}}// end m, l loop
                                 for (l=k+1; l<fArray[h].length; l++) {tempTime=[]; tempTime2=[];
                                 for (m=0; m<shiftsPair.length/2; m++){
                                   for (n=0; n<fArray[h][l][i].length/2; n++) {if ((fArray[h][l][i][2*n]>=shiftsPair[2*m])&&(fArray[h][l][i][2*n]<shiftsPair[2*m+1])){
-                                    if (fArray[h][l][i][2*n+1]>=shiftsPair[2*m+1]) {tempTime.push(fArray[h][l][i][2*n], shiftsPair[2 * m + 1])}
+                                    if (fArray[h][l][i][2*n+1]>=shiftsPair[2*m+1]) {tempTime.push(fArray[h][l][i][2*n], shiftsPair[2*m+1])}
                                     else { tempTime.push(fArray[h][l][i][2*n], fArray[h][l][i][2*n+1]) }}
-                                    else if (fArray[h][l][i][2 * n] <= shiftsPair[2 * m] && fArray[h][l][i][2 * n + 1] > shiftsPair[2 * m]) {
-                                      if (fArray[h][l][i][2 * n + 1] >= shiftsPair[2 * m + 1]) { tempTime.push(shiftsPair[2 * m], shiftsPair[2 * m + 1]) }
-                                      else { tempTime.push(shiftsPair[2 * m], fArray[h][l][i][2 * n + 1])}
+                                    else if (fArray[h][l][i][2*n]<=shiftsPair[2*m]&&fArray[h][l][i][2*n+1]>shiftsPair[2*m]) {
+                                      if (fArray[h][l][i][2*n+1]>=shiftsPair[2*m+1]) { tempTime.push(shiftsPair[2*m], shiftsPair[2*m+1])}
+                                      else { tempTime.push(shiftsPair[2*m], fArray[h][l][i][2*n+1])}
                                     }}} // end second m loop
-                                for (m = 0; m < fiveOnFive5[h][i].length/2; m++) { for (n = 0; n < tempTime.length/2; n++) {if ((tempTime[2*n]>=fiveOnFive5[h][i][2*m])&&(tempTime[2*n]<=fiveOnFive5[h][i][2*m+1])){
+                                for (m=0;m<fiveOnFive5[h][i].length/2; m++) { for (n=0;n<tempTime.length/2;n++) {if ((tempTime[2*n]>=fiveOnFive5[h][i][2*m])&&(tempTime[2*n]<=fiveOnFive5[h][i][2*m+1])){
                                   if (tempTime[2*n+1] >= fiveOnFive5[h][i][2*m+1]) {tempTime2.push(fiveOnFive5[h][i][2*m+1]-tempTime[2*n])}
                                   else {tempTime2.push(tempTime[2*n+1]-tempTime[2*n])}}
                                 else if ((tempTime[2*n] <= fiveOnFive5[h][i][2*m])&&(tempTime[2*n+1] >= fiveOnFive5[h][i][2*m])) {
